4aefc6b278a6132913d46df84e2550f67f6788c5
James Lucassen <james.z.lucassen@gmail.com>
2020-11-07 16:17:01 -0800

    got colors to show

--- maskSim.py
+++ maskSim.py
@@ -3,9 +3,31 @@ import statistics as s
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import numpy as np
+import numpy as np     
+import matplotlib.cm as cm
 
-def _update_plot(i, fig, scatter):
+# Set up population
+popsize = 50
+pop = np.zeros(popsize, dtype=[('pos', float, 2), ('mom', float, 2), ('clr', float, 1)])
+pop['pos'] =  np.random.uniform(-1, 1, (popsize, 2))
+pop['mom'] =  np.random.uniform(-1, 1, (popsize, 2))
+
+pop[0]['clr'] = 1
+
+# Set up figure
+fig =  plt.figure()
+ax = fig.add_subplot(111)
+ax.set_xlim([-1, 1])
+ax.set_ylim([-1, 1])
+
+# Plot population on figure
+scat = plt.scatter(pop['pos'][0], pop['pos'][1])
+scat.set_array(pop['clr'])
+scat.set_cmap('coolwarm')
+scat.set_alpha(0.8)
+
+# Frame update function
+def _update_plot(i, fig, scat):
     drift = 0.01
     for ind in pop:
         ind['mom'][0] += r.uniform(-drift, drift)
@@ -20,25 +42,9 @@ def _update_plot(i, fig, scatter):
             ind['mom'][1] = abs(ind['mom'][1])
         ind['pos'][0] += drift*ind['mom'][0] + r.uniform(-drift, drift)
         ind['pos'][1] += drift*ind['mom'][1] + r.uniform(-drift, drift)
-    scatter.set_offsets(pop['pos'])
-    return scatter        
+    scat.set_offsets(pop['pos'])
+    return scat   
 
-popsize = 50
-pop = np.zeros(popsize, dtype=[('pos', float, 2), ('mom', float, 2), ('col', float, 4)])
-pop['pos'] =  np.random.uniform(-1, 1, (popsize, 2))
-pop['mom'] =  np.random.uniform(-1, 1, (popsize, 2))
-
-
-# Set up figure
-fig =  plt.figure()
-ax = fig.add_subplot(111)
-ax.set_xlim([-1, 1])
-ax.set_ylim([-1, 1])
-
-scatter = plt.scatter(pop['pos'][0], pop['pos'][1], c = pop['col'])
-scatter.set_alpha(0.8)
-
-anim = animation.FuncAnimation(fig, _update_plot, fargs = (fig, scatter), frames = 100, interval = 100)          
+# Animate
+anim = animation.FuncAnimation(fig, _update_plot, fargs = (fig, scat), frames = 100, interval = 100)          
 plt.show()
- 
-
