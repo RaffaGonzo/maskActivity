860d3ceddaaa52075b031e37ed367d9e97ced0aa
James Lucassen <james.z.lucassen@gmail.com>
2020-11-07 18:50:24 -0800

    added risk histogram

--- maskSim.py
+++ maskSim.py
@@ -51,11 +51,19 @@ def change_clr(idx):
 
     scat.set_array(pop['clr'])
 
+def change_hist():
+    ax2.cla()
+    ax2.set_xlim([0, 1])
+    ax2.set_ylim([0, popsize])
+    ax2.hist(pop['clr'], bins=np.arange(0,1,0.05))
+    plt.draw()
+
 # Function to run between each frame
 def _update_plot(i, fig, scatter):
     change_pos()
     change_clr(i)
-    return scat
+    change_hist()
+    return
 
 # Set up population
 popsize = 50
@@ -65,16 +73,22 @@ pop['mom'] = np.random.uniform(-1, 1, (popsize, 2))
 pop['clr'] = np.random.uniform(0.03, 0.05, popsize)
 
 # Set up figures
-fig = plt.figure(1)
-ax = fig.add_subplot(111)
+fig = plt.figure()
+ax = fig.add_subplot(121)
 ax.set_xlim([-1, 1])
 ax.set_ylim([-1, 1])
+ax2 = fig.add_subplot(122)
+ax2.set_xlim([0, 1])
+ax2.set_ylim([0, popsize])
 
-# Initial plot
-scat = plt.scatter(pop['pos'][0], pop['pos'][1])
+# Initial arena plot
+scat = ax.scatter(pop['pos'][0], pop['pos'][1])
 scat.set_array(pop['clr'])
 scat.set_cmap('coolwarm')
 scat.set_alpha(0.8)
+
+# Initial histogram
+hist = ax2.hist(pop['clr'], bins=np.arange(0,1,0.05))
 
 # Animate
 anim = animation.FuncAnimation(fig,
