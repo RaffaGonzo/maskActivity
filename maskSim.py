6ed431cb6ec49463eac1e54b15ab730d34c6a563
James Lucassen <james.z.lucassen@gmail.com>
2020-11-07 16:46:37 -0800

    color spreading, eternal spreader test

--- maskSim.py
+++ maskSim.py
@@ -7,6 +7,7 @@ import matplotlib.animation as animation
 from matplotlib import colors as clr
 import matplotlib.cm as cm
 
+# Drifts momentum randomly, updates position based on momentum and more drift
 def change_pos():
     drift = 0.01
     for ind in pop:
@@ -22,25 +23,30 @@ def change_pos():
             ind['mom'][1] = abs(ind['mom'][1])
         ind['pos'][0] += drift * ind['mom'][0] + r.uniform(-drift, drift)
         ind['pos'][1] += drift * ind['mom'][1] + r.uniform(-drift, drift)
+    scat.set_offsets(pop['pos'])
 
+# Change color test
 def change_clr():
     trigger_radius = 0.06
-    risk_num = 0.5
+    risk_num = 0.99
     euclidean_dist = lambda x, y: sqrt((x**2) + (y**2))
     point = lambda x1, x2: abs(x1 - x2)
     for ind1 in pop:
         for ind2 in pop:
-            if euclidean_dist(point(ind1['pos'][0], ind2['pos'][1]),
-                              point(ind2['pos'][0], ind2['pos'][1])) <= trigger_radius:
-                ind1['clr'] += risk_num
-                ind2['clr'] += risk_num
+            if euclidean_dist(point(ind1['pos'][0], ind2['pos'][0]),
+                              point(ind1['pos'][1], ind2['pos'][1])) <= trigger_radius:
+                newColor = (ind1['clr'] + ind2['clr'])/2
+                ind1['clr'] = newColor
+                pop[0]['clr'] = 1 # eternal source
+                ind2['clr'] = newColor
+                pop[0]['clr'] = 1 # eternal source
+    scat.set_array(pop['clr'])
 
+# Function to run between each frame
 def _update_plot(i, fig, scatter):
-    change_pos()  # updates positions
+    change_pos()
     change_clr()
-    scat.set_offsets(pop['pos'])
     return scat
-
 
 # Set up population
 popsize = 50
@@ -48,6 +54,7 @@ pop = np.zeros(popsize, dtype=[('pos', float, 2), ('mom', float, 2), ('clr', flo
 pop['pos'] = np.random.uniform(-1, 1, (popsize, 2))
 pop['mom'] = np.random.uniform(-1, 1, (popsize, 2))
 
+# Patient zero
 pop[0]['clr'] = 1
 
 # Set up figure
@@ -56,17 +63,17 @@ ax = fig.add_subplot(111)
 ax.set_xlim([-1, 1])
 ax.set_ylim([-1, 1])
 
-# Plot population on figure
+# Initial plot
 scat = plt.scatter(pop['pos'][0], pop['pos'][1])
 scat.set_array(pop['clr'])
 scat.set_cmap('coolwarm')
 scat.set_alpha(0.8)
 
-anim = animation.FuncAnimation(fig, _update_plot,
+# Animate
+anim = animation.FuncAnimation(fig,
+                               _update_plot,
                                fargs=(fig, scat),
                                frames=100,
-                               interval=100)
+                               interval=50)
 
-# Animate
-anim = animation.FuncAnimation(fig, _update_plot, fargs = (fig, scat), frames = 100, interval = 100)
 plt.show()
